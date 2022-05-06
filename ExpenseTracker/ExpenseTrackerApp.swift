5cb8c1bb5041bcee168fa7ad0fc42fd7c8000f51
practice20200 <wor9hard2018@gmail.com>
2022-05-05 23:07:52 -0700

    initial commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Apple New on 2022-05-05.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
