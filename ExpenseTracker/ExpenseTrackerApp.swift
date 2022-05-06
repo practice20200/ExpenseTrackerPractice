25cf811540e74b7122f859c28694200fc302f983
practice20200 <wor9hard2018@gmail.com>
2022-05-06 12:33:15 -0700

    Modified TransactionViewmodel

--- ExpenseTracker/ExpenseTrackerApp.swift
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -1,17 +1,15 @@
-//
-//  ExpenseTrackerApp.swift
-//  ExpenseTracker
-//
-//  Created by Apple New on 2022-05-05.
-//
+
 
 import SwiftUI
 
 @main
 struct ExpenseTrackerApp: App {
+    @StateObject var transactionListVM = TransactionListViewModel()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(transactionListVM)
         }
     }
 }
--- ExpenseTracker/Models/Transaction.swift
+++ ExpenseTracker/Models/Transaction.swift
@@ -1,7 +1,7 @@
 
 
 import Foundation
-struct Transaction: Identifiable {
+struct Transaction: Identifiable, Decodable {
     let id: Int
     let date: String
     let institution: String
